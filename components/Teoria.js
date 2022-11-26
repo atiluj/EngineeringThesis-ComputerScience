1a56be32510ab4833240ee41357f75467ee2899d
atiluj <julita.osman@gmail.com>
2022-11-27 00:36:32 +0100

    remember user position

--- components/Teoria.js
+++ components/Teoria.js
@@ -13,12 +13,24 @@ import Lessons from '../lessons/teoria/config.jsx';
 const NavContext = React.createContext({
     curChapter: 0,
     curSubchapter: 0,
-    changeChapter: (chapter, subchapter) => () => {},
+    changeChapter: (chapter, subchapter) => { },
 });
 
+function Subchapter({ chapterId, subchapterId, subchapter }) {
+    const { changeChapter, curChapter, curSubchapter } = React.useContext(NavContext);
+    const isCurrent = chapterId == curChapter && subchapterId == curSubchapter;
+    return <a href="#" className={"subtopic" + (isCurrent ? " subtopic-current" : "")}
+        onClick={() => changeChapter(chapterId, subchapterId)}>
+        {chapterId}.{subchapterId + 1}. {subchapter.name}
+    </a>;
+}
+
 function ChapterWithSubchapters({ chapter, chapterId }) {
+    const { curChapter } = React.useContext(NavContext);
     const [show, setShow] = React.useState(false); //domyślnie schowane
-    const {changeChapter} = React.useContext(NavContext);
+    React.useEffect(() => {
+        if (curChapter == chapterId) setShow(true);
+    }, [curChapter]);
     return <div>
         <div className="topic" onClick={() => setShow(show => !show)}>
             <a>{chapterId}. {chapter.name}</a>
@@ -26,17 +38,16 @@ function ChapterWithSubchapters({ chapter, chapterId }) {
         </div>
         <Collapse in={show}>
             {chapter.subchapters.map((subchapter, subchapterId) =>
-                <a key={subchapterId}
-                    href="#" className="subtopic"
-                    onClick={changeChapter(chapterId, subchapterId)}>{subchapter.name}</a>
+                <Subchapter key={subchapterId}
+                    chapterId={chapterId} subchapterId={subchapterId} subchapter={subchapter} />
             )}
         </Collapse>
     </div>;
 }
 
 function ChapterStandalone({ chapter, chapterId }) {
-    const {changeChapter} = React.useContext(NavContext);
-    return <div onClick={changeChapter(chapterId, 0)}>
+    const { changeChapter } = React.useContext(NavContext);
+    return <div onClick={() => changeChapter(chapterId, 0)}>
         <div className="topic">
             <a>{chapterId}. {chapter.name}</a>
             <div style={{ visibility: "hidden" }}><ExpandMore /></div>
@@ -53,7 +64,7 @@ function Chapter({ chapter, chapterId }) {
 }
 
 function Contents() {
-    const {curChapter, curSubchapter} = React.useContext(NavContext);
+    const { curChapter, curSubchapter } = React.useContext(NavContext);
     const chapter = Lessons[curChapter];
     const subchapter = chapter.subchapters ? chapter.subchapters[curSubchapter] : chapter;
     let title = `${curChapter}.`;
@@ -71,10 +82,9 @@ export default function () {
     const [curChapter, setCurChapter] = React.useState(0);
     const [curSubchapter, setCurSubchapter] = React.useState(0);
     function changeChapter(ch, subch) {
-        return () => {
-            setCurChapter(ch);
-            setCurSubchapter(subch);
-        };
+        setCurChapter(ch);
+        setCurSubchapter(subch);
+        window.location.hash = `${ch}.${subch + 1}`;
     }
 
     const maxChapter = Lessons.length - 1;
@@ -82,57 +92,70 @@ export default function () {
     const prevLessonExists = curChapter > 0 || curSubchapter > 0;
     const nextLessonExists = curChapter != maxChapter || curSubchapter != maxSubchapter(curChapter);
 
+    React.useEffect(() => {
+        if (window.location.hash.length > 0) {
+            const parts = window.location.hash.replace('#', '').split('.');
+            if (parts.length == 2) {
+                const chapter = Number(parts[0]);
+                const subchapter = Number(parts[1]) - 1;
+                if (chapter >= 0 && chapter <= maxChapter
+                    && subchapter >= 0 && subchapter <= maxSubchapter(chapter)) {
+                    setCurChapter(chapter);
+                    setCurSubchapter(subchapter);
+                }
+            }
+        }
+    }, []);
+
     function prevLesson() {
         if (curSubchapter == 0) {
             const prevChapter = curChapter - 1;
-            setCurChapter(prevChapter);
-            setCurSubchapter(Lessons[prevChapter].subchapters ? Lessons[prevChapter].subchapters.length - 1 : 0);
+            changeChapter(prevChapter, maxSubchapter(prevChapter));
         } else {
-            setCurSubchapter(curSubchapter - 1);
+            changeChapter(curChapter, curSubchapter - 1);
         }
     }
 
     function nextLesson() {
-        if (!Lessons[curChapter].subchapters || curSubchapter == Lessons[curChapter].subchapters.length - 1) {
-            setCurChapter(curChapter + 1);
-            setCurSubchapter(0);
+        if (curSubchapter == maxSubchapter(curChapter)) {
+            changeChapter(curChapter + 1, 0);
         } else {
-            setCurSubchapter(curSubchapter + 1);
+            changeChapter(curChapter, curSubchapter + 1);
         }
     }
 
-    return <NavContext.Provider value={{curChapter, curSubchapter, changeChapter}}>
+    return <NavContext.Provider value={{ curChapter, curSubchapter, changeChapter }}>
         <main>
-        <div className="contents">
-            <div className="title ft">
-                <a>TEORIA</a>
-            </div>
-            <div className="cont-title">
-                <a>SPIS TREŚCI</a>
-            </div>
+            <div className="contents">
+                <div className="title ft">
+                    <a>TEORIA</a>
+                </div>
+                <div className="cont-title">
+                    <a>SPIS TREŚCI</a>
+                </div>
 
-            <div className="cont">
-                {Lessons.map((chapter, chapterId) =>
-                    <Chapter key={chapterId}
-                        chapter={chapter}
-                        chapterId={chapterId} />
-                )}
-            </div>
-        </div>
-
-        <div className="main">
-            <div className="title">
-                <a>TEORIA</a>
-            </div>
-            <Contents />
-            <div className='controls'>
-                <div><label><input type="checkbox" />OZNACZ JAKO PRZEROBIONĄ</label></div>
-                <div>
-                    <button onClick={prevLesson} style={{ visibility: prevLessonExists ? "visible" : "hidden" }}><ArrowBackIosNewIcon /></button>
-                    <button onClick={nextLesson} style={{ visibility: nextLessonExists ? "visible" : "hidden" }}><ArrowForwardIosIcon /></button>
+                <div className="cont">
+                    {Lessons.map((chapter, chapterId) =>
+                        <Chapter key={chapterId}
+                            chapter={chapter}
+                            chapterId={chapterId} />
+                    )}
                 </div>
             </div>
-        </div>
-    </main>
+
+            <div className="main">
+                <div className="title">
+                    <a>TEORIA</a>
+                </div>
+                <Contents />
+                <div className='controls'>
+                    <div><label><input type="checkbox" />OZNACZ JAKO PRZEROBIONĄ</label></div>
+                    <div>
+                        <button onClick={prevLesson} style={{ visibility: prevLessonExists ? "visible" : "hidden" }}><ArrowBackIosNewIcon /></button>
+                        <button onClick={nextLesson} style={{ visibility: nextLessonExists ? "visible" : "hidden" }}><ArrowForwardIosIcon /></button>
+                    </div>
+                </div>
+            </div>
+        </main>
     </NavContext.Provider>;
 }
