de4387c536b3858e94ec616ce7f9099adf2bd5fd
Ola1510 <58330694+Ola1510@users.noreply.github.com>
2022-12-26 23:11:47 +0000

    Dodaje lekcje z Accessa, teorie Baz Danych i SQL

--- lessons/access/config.jsx
+++ lessons/access/config.jsx
@@ -1,17 +1,32 @@
 import Wskazowki from './wskazowki.mdx';
+import Wlaczanie from './wlaczanie.mdx';
+import Importowanie from './importowanie.mdx';
+import Relacje from './relacje.mdx'; 
+
+import ProsteKwerendy from './prosteKwerendy.mdx';
+import ZlozoneKwerendy from './zlozoneKwerendy.mdx';
+import Wyrazenia from './wyrazenia.mdx';
+
+import Teoria from './teoria.mdx';
+import Sql from './sql.mdx';
+
 
 export default [
     {name: "Wskazówki", content: <Wskazowki />},
 
-    {name: "Modelowanie", subchapters: [
-        {name: "Importowanie danych", content: null},
+        {name: "Modelowanie", subchapters: [
+        {name: "Włącznie programu MS Access", content: <Wlaczanie/>},
+        {name: "Importowanie danych", content: <Importowanie/>},
+        {name: "Relacje między tabelami", content: <Relacje/>},
         {name: "Projektowanie relacyjnych baz danych", content: null},
         {name: "Tworzenie tabel", content: null},
-        {name: "Relacje między tabelami", content: null},
     ]},
 
     {name: "Tworzenie zapytań", subchapters: [
-        {name: "Tworzenie kwerend", content: null},
+        {name: "Tworzenie prostych kwerend", content: <ProsteKwerendy/>},
+        {name: "Tworzenie kwerend wykorzystujących kilka tabel", content: <ZlozoneKwerendy/>},
+        {name: "Konstruuowanie wyrażeń", content: <Wyrazenia/>},
+        {name: "Kwerendy pomocnicze", content: null},
         {name: "Tworzenie raportów", content: null},
     ]},
 
@@ -22,9 +37,9 @@ export default [
     ]},
 
     {name: "SQL", subchapters: [
-        {name: "Składnia", content: null},
+        {name: "Składnia", content: <Sql/>},
         {name: "Tworzenie zapytań", content: null},
         {name: "Tworzenie raportów", content: null},
-        {name: "Teoria baz danych*", content: null},
+        {name: "Teoria baz danych*", content: <Teoria/>},
     ]},
 ];
