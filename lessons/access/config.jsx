1751ca8830d51a18860af6cec2d571a857199683
Ola1510 <58330694+Ola1510@users.noreply.github.com>
2022-12-27 19:54:11 +0000

    Adding new topics to Access

--- lessons/access/config.jsx
+++ lessons/access/config.jsx
@@ -1,4 +1,6 @@
 import Wskazowki from './wskazowki.mdx';
+import Wstep from './wstep.mdx';
+
 import Wlaczanie from './wlaczanie.mdx';
 import Importowanie from './importowanie.mdx';
 import Relacje from './relacje.mdx'; 
@@ -6,6 +8,10 @@ import Relacje from './relacje.mdx';
 import ProsteKwerendy from './prosteKwerendy.mdx';
 import ZlozoneKwerendy from './zlozoneKwerendy.mdx';
 import Wyrazenia from './wyrazenia.mdx';
+import KwerendyPomocnicze from './kwerendyPomocnicze.mdx';
+
+import Niedopasowane from './niedopasowane.mdx';
+import Duplikaty from './duplikaty.mdx'
 
 import Teoria from './teoria.mdx';
 import Sql from './sql.mdx';
@@ -13,6 +19,7 @@ import Sql from './sql.mdx';
 
 export default [
     {name: "Wskazówki", content: <Wskazowki />},
+    {name: "Wstęp", content: <Wstep />},
 
         {name: "Modelowanie", subchapters: [
         {name: "Włącznie programu MS Access", content: <Wlaczanie/>},
@@ -26,12 +33,13 @@ export default [
         {name: "Tworzenie prostych kwerend", content: <ProsteKwerendy/>},
         {name: "Tworzenie kwerend wykorzystujących kilka tabel", content: <ZlozoneKwerendy/>},
         {name: "Konstruuowanie wyrażeń", content: <Wyrazenia/>},
-        {name: "Kwerendy pomocnicze", content: null},
+        {name: "Kwerendy pomocnicze", content: <KwerendyPomocnicze/>},
         {name: "Tworzenie raportów", content: null},
     ]},
 
     {name: "Zaawansowane funkcje", subchapters: [
-        {name: "Wyszukiwanie danych po przez odwórcenie problemów + wyjątki", content: null},
+        {name: "Wyszukiwanie danych po przez odwórcenie problemów + wyjątki", content: <Niedopasowane/>},
+        {name: "Wyszukiwanie duplikatów", content: <Duplikaty/>},
         {name: "Agregowanie danych", content: null},
         {name: "Kwerendy krzyżowe", content: null},
     ]},
