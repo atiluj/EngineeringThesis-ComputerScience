f7384de987500221afc780704b618e06ef0e8b87
atiluj <julita.osman@gmail.com>
2022-12-30 15:18:29 +0100

    add exercise page, add description

---
+++ components/Exercise.jsx
@@ -0,0 +1,92 @@
+import './Exercise.css';
+
+export default function () {
+    return <>
+        <main>
+            <div className="introduction"><p>W tej podstronie znajdziesz bezpośrednie przekierowania do zadań programistycznych oraz sprawdzaczki,
+                czyli maszynki, która przemieli twoje rozwiązanie i zwróci informację o tym czy przechodzi on testy. rozwiązanie
+                to ma na celu ułatwić Ci naukę blah blah blah</p></div>
+            <div className="exercises-title">
+                <h1>ZADANIA Z KURSU PYTHON</h1>
+                <p>W tym dziale są zdania, które znajdziesz również w kursie. Posiadają one rozwiązania wzrocowe, które mają
+                    za zadanie wprowadzić Cię w programowanie.</p>
+            </div>
+
+            <div className="exercises-list">
+                <div className="list-element">1</div>
+                <div className="list-element">2</div>
+                <div className="list-element">3</div>
+                <div className="list-element">4</div>
+                <div className="list-element">5</div>
+                <div className="list-element">6</div>
+                <div className="list-element">7</div>
+                <div className="list-element">8</div>
+                <div className="list-element">9</div>
+                <div className="list-element">10</div>
+                <div className="list-element">11</div>
+                <div className="list-element">12</div>
+                <div className="list-element">13</div>
+                <div className="list-element">14</div>
+                <div className="list-element">15</div>
+                <div className="list-element">16</div>
+                <div className="list-element">17</div>
+                <div className="list-element">18</div>
+                <div className="list-element">19</div>
+                <div className="list-element">20</div>
+
+            </div>
+
+            <div className="exercises-title">
+                <h1>POZSOTAŁE ZADANIA</h1>
+                <p>Przerobienie ponizszych zadań jest gwarantem opanowania Pythona oraz uzyskania dobrego wyniku na maturze.
+                    na podstawie matur coś tam blabla bla.
+                </p>
+            </div>
+
+            <div className="exercises-list">
+                <div className="list-element"><p>Matura 2022 z.1</p></div>
+                <div className="list-element"><p>Matura 2022 z.2</p></div>
+                <div className="list-element"><p>Matura 2021 z.1</p></div>
+                <div className="list-element"><p>Matura 2021 z.1</p></div>
+                <div className="list-element"><p>Matura 2020 z.1</p></div>
+                <div className="list-element"><p>Matura 2020 z.1</p></div>
+                <div className="list-element"><p>Matura 2019 z.1</p></div>
+                <div className="list-element"><p>Matura 2019 z.1</p></div>
+                <div className="list-element"><p>Matura 2018 z.1</p></div>
+                <div className="list-element"><p>Matura 2018 z.1</p></div>
+                <div className="list-element"><p>Matura 2017 z.1</p></div>
+                <div className="list-element"><p>Matura 2017 z.1</p></div>
+                <div className="list-element"><p>Matura 2016 z.1</p></div>
+                <div className="list-element"><p>Matura 2016 z.1</p></div>
+                <div className="list-element"><p>Matura 2018 z.1</p></div>
+                <div className="list-element"><p>Matura 2018 z.1</p></div>
+                <div className="list-element"><p>Matura 2017 z.1</p></div>
+                <div className="list-element"><p>Matura 2017 z.1</p></div>
+                <div className="list-element"><p>Matura 2016 z.1</p></div>
+                <div className="list-element"><p>Matura 2016 z.1</p></div>
+            </div>
+
+            <div className="category">SORTOWANIE</div>
+            <div className="exercises-list">
+                <div className="list-element"><p>SORTOWANIE Z.1</p></div>
+                <div className="list-element"><p>SORTOWANIE Z.2</p></div>
+                <div className="list-element"><p>SORTOWANIE Z.3</p></div>
+                <div className="list-element"><p>SORTOWANIE Z.4</p></div>
+                <div className="list-element"><p>SORTOWANIE Z.5</p></div>
+                <div className="list-element"><p>SORTOWANIE Z.6</p></div>
+                <div className="list-element"><p>SORTOWANIE Z.7</p></div>
+                <div className="list-element"><p>SORTOWANIE Z.8</p></div>
+            </div>
+
+            <div className="category">SZYFROWANIE</div>
+            <div className="exercises-list">
+                <div className="list-element"><p>SZYFROWANIE Z.1</p></div>
+                <div className="list-element"><p>SZYFROWANIE Z.2</p></div>
+                <div className="list-element"><p>SZYFROWANIE Z.3</p></div>
+                <div className="list-element"><p>SZYFROWANIE Z.4</p></div>
+                <div className="list-element"><p>SZYFROWANIE Z.5</p></div>
+            </div>
+
+        </main>
+    </>;
+}
