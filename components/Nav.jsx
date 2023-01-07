7136d7c89edf22446c92b21f87cb50b2f3404314
Ola1510 <58330694+Ola1510@users.noreply.github.com>
2023-01-07 01:20:50 +0000

    drop-down button work in progress

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,6 +1,5 @@
 import './Nav.css';
 import * as React from 'react';
-import LightModeIcon from '@mui/icons-material/LightMode';
 
 function useWindowWidth() {
     const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
@@ -34,36 +33,50 @@ export default function() {
     const windowWidth = useWindowWidth();
     const scrollY = useScrollY();
 
-    let padding, fontSize;
-    if (windowWidth > 1200) {
-        if (scrollY > 80) {
-            padding = "7px 17px";
-            fontSize = "20px";
-        } else {
-            padding = "16px 23px";
-            fontSize = "18px";
-        }
-    } else if (windowWidth > 1000) {
-        if (scrollY > 80) {
-            padding = "9px 17px";
-            fontSize = "14px";
-        } else {
-            padding = "13px 17px";
-            fontSize = "18px";
-        }
-    } else if (windowWidth > 810) {
-        if (scrollY > 80) {
-            padding = "12px 10px";
-            fontSize = "12px";
-        } else {
-            padding = "15px 10px";
-            fontSize = "14px";
-        }
+    //TODO: drop-down menu
+    function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
     }
+    }
+    //
+
+    let padding, fontSize;
+    // if (windowWidth > 1200) {
+    //     if (scrollY > 80) {
+    //         padding = "7px 17px";
+    //         fontSize = "20px";
+    //     } else {
+    //         padding = "16px 23px";
+    //         fontSize = "18px";
+    //     }
+    // } else if (windowWidth > 1000) {
+    //     if (scrollY > 80) {
+    //         padding = "9px 17px";
+    //         fontSize = "14px";
+    //     } else {
+    //         padding = "13px 17px";
+    //         fontSize = "18px";
+    //     }
+    // } else if (windowWidth > 810) {
+    //     if (scrollY > 80) {
+    //         padding = "12px 10px";
+    //         fontSize = "12px";
+    //     } else {
+    //         padding = "15px 10px";
+    //         fontSize = "14px";
+    //     }
+    // }
 
     const aStyle = {padding, fontSize};
 
     return <nav id="navbar" className="gradient">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+
             <div id="nav-wrapper-left">
                
                 <a href="index.html" style={aStyle} className="menu-button nav-button">Cześć!</a>
@@ -85,6 +98,10 @@ export default function() {
                         <input type="checkbox"/>
                         <span className="slider round"></span>
                 </label>
+
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <i class="fa fa-bars"></i>
+                </a>
         
                     
             </div>
