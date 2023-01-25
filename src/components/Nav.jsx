a8d7825225451daedcd5d7911ee50eda572638f7
atiluj <julita.osman@gmail.com>
2023-01-25 23:20:07 +0100

    fixed navigation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 import styles from './Nav.module.css';
-import '../style.css'; 
+import '../style.css';
 
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Outlet, Link } from "react-router-dom";
 import MenuIcon from '@mui/icons-material/Menu';
 import { style } from '@mui/system';
@@ -19,7 +19,7 @@ const useDarkMode = () => {
     useEffect(() => {
         localStorage.setItem(DARK_MODE_KEY, darkMode);
         document.documentElement.setAttribute('data-theme', darkMode ? 'dark' : 'light');
-        window.postMessage({darkMode}, window.location.origin);
+        window.postMessage({ darkMode }, window.location.origin);
     }, [darkMode])
 
     return [darkMode, setDarkMode]
@@ -85,15 +85,15 @@ function Nav() {
         }
     }
 
-    const aStyle = {padding, fontSize};
+    const aStyle = { padding, fontSize };
 
     function navBurger() {
         // TODO: refactor
         const x = document.getElementById("top_navbar");
-        if (x.className === `gradient ${styles.navbar}`) {
+        if (x.className === `gradient ${styles.navbar_sticky}`) {
             x.className += ` ${styles.responsive}`;
         } else {
-            x.className = `gradient ${styles.navbar}`;
+            x.className = `gradient ${styles.navbar_sticky}`;
         }
     }
 
@@ -102,36 +102,38 @@ function Nav() {
     }
 
     return <>
-        <nav id="top_navbar" className={`gradient ${styles.navbar}`}>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+        <nav className={styles.navbar}>
+            <div id="top_navbar" className={`gradient ${styles.navbar_sticky}`}>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 
                 <div id="nav_wrapper_left" className={styles.nav_wrapper_left}>
-                
+
                     <Link to="/" style={aStyle} className={`menu_button nav_button ${styles.nav_button}`}>Cześć!</Link>
-                
+
                     <Link to="/teoria" style={aStyle} className={`menu_button nav_button ${styles.nav_button}`}>Teoria</Link>
-            
+
                     <Link to="/excel" style={aStyle} className={`menu_button nav_button ${styles.nav_button}`}>Excel</Link>
-                
+
                     <Link to="/access" style={aStyle} className={`menu_button nav_button ${styles.nav_button}`}>Access</Link>
-                
+
                     <Link to="/python" style={aStyle} className={`menu_button nav_button ${styles.nav_button}`}>Python</Link>
-                
+
                     <Link to="/exercise" style={aStyle} className={`menu_button nav_button ${styles.nav_button}`}>Zadania</Link>
                 </div>
                 <div id="nav_wrapper_right" className={styles.nav_wrapper_right}>
                     <Link to="/interpreter" style={aStyle} className={`menu_button nav_button ${styles.nav_button}`}>Interpreter python</Link>
 
                     <label className={styles.switch} >
-                            <input id="checkbox" type="checkbox" checked={darkMode} className={styles.checkbox} onChange={toggleMode}/>
-                            <span className={`${styles.slider} ${styles.round}`}></span>
+                        <input id="checkbox" type="checkbox" checked={darkMode} className={styles.checkbox} onChange={toggleMode} />
+                        <span className={`${styles.slider} ${styles.round}`}></span>
                     </label>
 
                     <button className={styles.icon} onClick={navBurger}>
-                        <MenuIcon className={styles.nav_burger}/>
-                    </button> 
+                        <MenuIcon className={styles.nav_burger} />
+                    </button>
                 </div>
-            
+            </div>
+
         </nav>
 
         <Outlet />
