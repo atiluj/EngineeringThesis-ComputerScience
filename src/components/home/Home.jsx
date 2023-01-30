b0e1c1b58015240227922b6d8cf434be8ea1e14e
atiluj <julita.osman@gmail.com>
2023-01-30 23:56:28 +0100

    text corrected - Home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -41,16 +41,15 @@ function Home() {
                             Na szczęście, każdego roku w internecie powstaje coraz więcej blogów czy filmów, które są
                             bardzo pomocne. Wciąż jednak istnieje mało kompleksowych kursów, które zbierają całą
                             potrzebną wiedzę w jednym miejscu i co najważniejsze są w 100% darmowe. Tworząc tę stronę
-                            kierowałyśmy własnymi doświadczeniami - same kilka lat temu przygotowywałyśmy się do tej
-                            matury i wiemy, czego nam wtedy brakowały. Chcemy oddać w Twoje ręce gotowe narzędzie, dzięki
-                            któremu oszczedzisz mnóstwo czasu i pieniędzy, a nauka będzie sprawna, przyjemna oraz
+                            kierowałyśmy własnymi doświadczeniami — same kilka lat temu przygotowywałyśmy się do tej
+                            matury i wiemy, czego nam wtedy brakowało. Chcemy oddać w Twoje ręce gotowe narzędzie, dzięki
+                            któremu oszczędzisz mnóstwo czasu i pieniędzy, a nauka będzie sprawna, przyjemna oraz
                             przyniesie oczekiwane rezultaty. Przekazujemy Ci też mnóstwo wskazówek, które ułatwią Ci pracę
-                            i powiedza na co zwrócić uwagę, a czego nie warto się uczyć.
-                            Jeśli nie miałeś wcześniej styczności z informatyką - nie przejmuj się - nasz kurs
+                            i powiedzą, na co zwrócić uwagę, a czego nie warto się uczyć.<br /><br />
+                            Jeśli nie miałeś wcześniej styczności z informatyką — nie przejmuj się — nasz kurs
                             nie wymaga żadnej wcześniejszej wiedzy i powoli wprowadzi Cię w każdy temat. Poziom
-                            trudności wykładów i zadań stopniowo wzrasta, a więc osoby bardziej zaawansowane na pewno
-                            znajdą tutaj coś dla siebie. Mamy nadzieję, że ta strona pomoże Ci w nauce i uporządkowaniu
-                            wiedzy.
+                            trudności wykładów i zadań stopniowo wzrasta, a więc i osoby bardziej zaawansowane na pewno
+                            znajdą tutaj coś dla siebie.
                         </p>
                     </article>
                     <article>
@@ -66,7 +65,7 @@ function Home() {
                         </ul>
                         <br />
                         <p>Każda lekcja zawiera teoretyczne wprowadzenie do danego zagadnienia, a także przykłady i ćwiczenia, dzięki którym będziesz mógł/mogła sprawdzić swoją wiedzę i utrwalić nowo poznany temat.
-                            W sekcji komenatarzy możesz zadać nam pytanie, na które postaramy się odpowiedzieć.</p>
+                            W sekcji komenatrzy możesz zadać nam pytanie, na które postaramy się odpowiedzieć.</p>
 
                     </article>
                     <article>
@@ -75,24 +74,40 @@ function Home() {
                             WYBIERZ SWÓJ POZIOM:<br /><br />
 
                             <b>JESZCZE NIE WIEM, ALE ZA MIESIĄC MUSZĘ TO ZDAĆ (niepolecany)</b><br />
-                            Został Ci miesiąc - nie ucz się teorii, nie warto!
-                            Jeśli masz już podstawy wiedzy programistycznej to szlifuj tę umiejśtność najbardziej jak to możliwe. Przerób wszystkie zadania programistyczne i algorytmiczne z poprzednich matur i przeczytaj materiały dostępne na strone. W wolnych chwilach dołącz do tego naukę Excela (i Accessa, jeśli podstawy Excela nie są Ci już obce).
-                            Jeśli natomiast zaczynasz od podstaw skup się na zadaniach z Excela i Accessa. Zadania programistycznie nie są tak łatwe, a nauka myślenia algorytmicznego wymaga czasu. Jeśli masz dużą motywację spróbuj opanować podstawy kodowania, ale nie trać czasu na wgłębianie się w trudneijsze podpunkty zadań czy zaawansowane funkcje Pythona.
+                            Został Ci miesiąc — nie ucz się teorii, nie warto!
+                            Jeśli masz już podstawy wiedzy programistycznej, to szlifuj tę umiejętność najbardziej jak to możliwe. 
+                            Przerób wszystkie zadania programistyczne i algorytmiczne z poprzednich matur i przeczytaj materiały dostępne na stronę. 
+                            W wolnych chwilach dołącz do tego naukę Excela (i Accessa, jeśli podstawy Excela nie są Ci już obce).
+                            Jeśli natomiast zaczynasz od podstaw to skup się na zadaniach z Excela i Accessa. 
+                            Zadania programistycznie nie są tak łatwe, a nauka myślenia algorytmicznego wymaga czasu. 
+                            Jeśli masz dużą motywację spróbuj opanować podstawy kodowania, ale nie trać czasu na wgłębianie się w trudniejsze 
+                            podpunkty zadań czy zaawansowane funkcje Pythona.
                             <br /><br />
                             <b>JESTEM ZIELONY</b><br />
-                            Jeśli zaczynasz od podstaw, to masz dwie możliwości. Jedna z nich to rozpoczęcie nauki od prostych algorytmów i przejście do nauki programowania krok po kroku. Druga to droga od zera do mistrza pakietu office, czyli dużo ćwiczeń z Excela i Accessa.
-                            Oczywiście najepiej nauczyć się obu tych części i zdobyć kompelksową wiedzę. Z doświadczenia wiemy, że opanowanie umiejętności programistycznych nie jest łatwe i wymaga więcej czasu. Muisz bowiem przyzwyczaić się do algorytmicznego sposobu myślenia i rozwiązywania zadań poprzez analizowanie coraz mniejszych jego podproblemów. Dobrym pomysłem jest więc nauka algorytmów równolegle do zdobywania umiejętności związanych z Excelem czy Accessem. Różnorodna sepcyfika tych zadań pozwoli Ci nie wpaść w monotonnie i zwiększy wydajność procesu nauki. Oczywiście ważnym aspektem jest dostępność pakietów Microsoft, wskazówki na ten temat znajdziesz w zakładkach "Excel - wskazówki" oraz "Access - wskazówki".
+                            Jeśli zaczynasz od podstaw, to masz dwie możliwości. Jedna z nich to rozpoczęcie nauki od prostych algorytmów i 
+                            przejście do nauki programowania krok po kroku. Druga to droga od zera do mistrza pakietu office, czyli dużo ćwiczeń z 
+                            Excela i Accessa. Oczywiście najlepiej nauczyć się obu tych części i zdobyć kompleksową wiedzę. Z doświadczenia wiemy, 
+                            że opanowanie umiejętności programistycznych nie jest łatwe i wymaga więcej czasu. Musisz bowiem przyzwyczaić się do 
+                            algorytmicznego sposobu myślenia i rozwiązywania zadań poprzez analizowanie coraz mniejszych jego podproblemów. Dobrym 
+                            pomysłem jest więc nauka algorytmów równolegle do zdobywania umiejętności związanych z Excelem czy Accessem. Różnorodna 
+                            specyfika tych zadań pozwoli Ci nie wpaść w monotonię i zwiększy wydajność procesu nauki. Oczywiście ważnym aspektem jest 
+                            dostępność pakietów Microsoft, wskazówki na ten temat znajdziesz w zakładkach "Excel — wskazówki" oraz "Access — wskazówki".
                             <br /><br />
                             <b>NIBY MAM INFORMATYKĘ W SZKOLE, ALE TA SZKOŁA JEST W POLSCE</b><br />
-                            Jeśli potrzebujesz nauczyć się danego zagadnienia od podstaw lub chcesz solidnie utrwalić wiedzę, to możesz przerabiać tematy po kolei. Jeśli jednak czujesz, że dany temat nie sprawia Ci trudności śmiało pomiń niektóre fragmenty lub podtematy.
+                            Jeśli potrzebujesz nauczyć się danego zagadnienia od podstaw lub chcesz solidnie utrwalić wiedzę, to możesz przerabiać tematy po kolei. 
+                            Jeśli jednak czujesz, że dany temat nie sprawia Ci trudności, śmiało pomiń niektóre fragmenty lub podtematy.
                             <br /><br />
                             <b>SPORO UMIEM, ALE NIE WIEM CZEGO NIE WIEM</b><br />
-                            Większość wykładów zawiera dołączone ćwiczenia, możesz więc sprawdzić czy jesteś już zaznajomiony z omawianymi zagadnieniami poprzez rozwiązanie tych zadań. Jeśli sprawią Ci one trudność warto abyś wrócił do począdku tematu i przeczytał teorię. Jeśli natomiast były one dla Ciebie łatwe, to prawdopodobnie możesz już przejść do kolejnych działów.
+                            Większość wykładów zawiera dołączone ćwiczenia, możesz więc sprawdzić, czy jesteś już zaznajomiony z omawianymi 
+                            zagadnieniami poprzez rozwiązanie tych zadań. Jeśli sprawią Ci one trudność warto, abyś wrócił do początku tematu i 
+                            przeczytał teorię. Jeśli natomiast były one dla Ciebie łatwe, to prawdopodobnie możesz już przejść do kolejnych działów.
                         </p><br /><br /><br /><br />
                         <b>DLA KAŻDEGO</b><br />
-                        <p>Na maturze  nie marnuj czasu na główkowanie nad trudnym podpunktem. Jeśli coś zajmuje Ci więcej niż 5min, a postęp pracy nie jest znaczący, zostaw to na koniec.
+                        <p>Na maturze  nie marnuj czasu na główkowanie nad trudnym podpunktem. Jeśli coś zajmuje Ci więcej niż 5min, a postęp 
+                            pracy nie jest znaczący, zostaw to na koniec.
 
-                            Pamiętaj, że ten kurs jest dla Ciebie i to Ty decydujesz jak z niego korzystasz. Dla nas ważne jest, aby pomóc Ci w wyzwaniu jakie przed Tobą stoi najlepiej jak to możliwe.</p>
+                            Pamiętaj, że ten kurs jest dla Ciebie i to Ty decydujesz, jak z niego korzystasz. Dla nas ważne jest, aby pomóc Ci w 
+                            wyzwaniu jakie, przed Tobą stoi najlepiej jak to możliwe.</p>
                     </article>
                     <article>
                         <h1 id="fourth">WSKAZÓWKI</h1>
