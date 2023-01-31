d20b8f8dde351ad2d92bd7319af4e43816a890ae
Ola1510 <st.aleksandra2000@gmial.com>
2023-01-31 20:00:41 +0100

    adding excel lessons

--- src/lessons/excel/config.jsx
+++ src/lessons/excel/config.jsx
@@ -4,8 +4,10 @@ import WlaczanieExcel from './wlaczanieExcel.mdx';
 import ImportExcel from './importExcel.mdx';
 
 import FormulyWbudowane from './formulyWbudowane.mdx';
+import SymboleWieloznaczne from './symbole.mdx';
 import FormulyLogiczne from './formulyLogiczne.mdx';
 import Przestawne from './przestawne.mdx';
+import Wyszukaj from './wyszukaj.mdx';
 import BledyExcel from './bledyExcel.mdx';
 
 import ZadaniaTeoriaExcel from './zadaniaTeoriaExcel.mdx';
@@ -21,10 +23,11 @@ const ExcelLessons = [
     {name: "Formuły", subchapters: [
         // {name: "Podstawy", content: null},
         {name: "Formuły wbudowane", content: <FormulyWbudowane/>},
+        {name: "Symbole wieloznaczne", content: <SymboleWieloznaczne/>},
         {name: "Formuły warunkowe", content: <FormulyLogiczne/>},
         // {name: "Formuły zaawansowane", content: null},
         {name: "Tabele i wykresy przestawne", content: <Przestawne/>},
-        // {name: "Wyszukaj pionowo/poziomo", content: null},
+        {name: "Wyszukaj pionowo/poziomo", content: <Wyszukaj/>},
         {name: "Rodzaje błędów", content: <BledyExcel/>},
     ]},
 
